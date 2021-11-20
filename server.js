1d5eaee7690cb66a88b639b4d32c4037f086d0b3
Jorge Barragan <jmarq19@outlook.com>
2021-11-20 10:03:12 -0800

    chaing the name of the route in the server file

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connect(
 
 // routes
 app.use(require("./routes/api.js"));
-app.use(require("./routes/html.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
